a5ef94fba8d0261fd1abfcf928b7a34e935e3479
HP <HP@DESKTOP-G4J7E8F>
2022-08-26 16:14:12 +0530

    Add project files.

---
+++ TemplatedHtmlFilter/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TemplatedHtmlFilter.Models;
+
+namespace TemplatedHtmlFilter.Controllers
+{
+    public class DemoController : Controller
+    {
+
+        // GET: Demo
+        public ActionResult Index()
+        {
+            Employee emp = new Employee()
+            {
+                Id = 1,
+                name = "Neha",
+                email = "neha@gmail.com",
+                IsOnline = false,
+                dob = DateTime.Now
+            };
+            return View(emp);
+        }
+
+        public ActionResult Edit()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ TemplatedHtmlFilter/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TemplatedHtmlFilter.Models
+{
+    public class Employee
+    {
+        
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        public string name { get; set; }
+
+        [Display(Name = "Email")]
+        public string email { get; set; }
+
+        [Display(Name = "Isonline")]
+        public bool IsOnline { get; set; }
+
+        [Display(Name = "Date Of Birth")]
+        [DataType(DataType.Date )]
+        public DateTime dob { get; set; }
+
+        [Display(Name = "Birth Time")]
+        [DataType(DataType.Time)]
+        public DateTime dobtime { get; set; }
+    }
+}
